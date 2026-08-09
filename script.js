2cb6c9bbe14faf2aab73326650bf20732c3b8344
Ajit Pandey <ajitytsurat0707@gmail.com>
2026-08-10 00:18:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
 // Your script here.
+// let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector("#speak");
+const stopButton = document.querySelector("#stop");
+const userText = document.querySelector("#userText");
+const rate = document.querySelector('[name="rate"]');
+const pitch = document.querySelector('[name="pitch"]');
+const selectedVoice = document.querySelector("#voices");
+const synth = window.speechSynthesis;
+let setPitchValue = 1,setRateValue = 1,userSelectVoice = "";
+
+synth.addEventListener("voiceschanged", (event) => {
+  const voiceList = synth.getVoices();
+  for (const voice of voiceList) {
+    console.log("voices ", voice);
+    let option = document.createElement("option");
+    let demoName = voice.name.substr(10, voice.name.length);
+    let i = 0;
+    let voiceName = "";
+    while (i < demoName.length && demoName[i] !== " ") {
+      voiceName += demoName[i];
+      i++;
+    }
+    option.textContent = `${voiceName} (${voice.lang})`;
+    option.setAttribute("data-name",voice.name);
+    voicesDropdown.appendChild(option);
+  }
+});
+
+
+selectedVoice.addEventListener("input", (event) => {
+  userSelectVoice = event.target.value;
+});
+
+pitch.addEventListener("change", (event) => {
+  setPitchValue = event.target.value;
+});
+
+rate.addEventListener("input", (event) => {
+  setRateValue = event.target.value;
+});
+
+speakButton.addEventListener("click", (event) => {
+  const msg = new SpeechSynthesisUtterance(userText.value);
+  msg.rate = setRateValue;
+  msg.pitch = setPitchValue;
+  let vList = synth.getVoices();
+  const selectedOption = document.querySelector("select").selectedOptions[0].getAttribute("data-name");
+  for(const vc of vList){
+    if(vc.name===selectedOption){
+      msg.voice = vc; 
+    }
+  }
+  synth.speak(msg);
+});
+
+stopButton.addEventListener("click", (event) => {
+  console.log("stop button ", event.target.textContent);
+  synth.pause();
+});
+
